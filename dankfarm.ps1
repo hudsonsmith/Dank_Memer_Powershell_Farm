7032df86fd472f44a234cec0df69e5cc399c8d78
hudson smith <>
2022-04-17 19:09:02 -0500

    Adding the dankfarm powershell script.

---
+++ dankfarm.ps1
@@ -0,0 +1,22 @@
+$wshell = New-Object -ComObject wscript.shell;
+
+$wshell.AppActivate('discord')
+
+while ($true) {	
+	$wshell.SendKeys('pls beg')
+	$wshell.SendKeys('~')
+	Sleep 1
+
+	$wshell.SendKeys('pls fish')
+	$wshell.SendKeys('~')
+	Sleep 1
+
+	$wshell.SendKeys('pls hunt')
+	$wshell.SendKeys('~')
+	Sleep 1
+
+	$wshell.SendKeys('pls dep all')
+	$wshell.SendKeys('~')
+	
+	Sleep 35
+}
